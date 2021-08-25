6bc2ed658851126bffbb1937c7622f9af2d71027
Vada Karlen <vada.karlen@gmail.com>
2021-08-24 23:50:34 -0500

    Reordered some file structure. Cleared out default content. Reset tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App/App';
 
-test('renders learn react link', () => {
+test('Renders landing page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText('Hello, world!');
+  expect(header).toBeInTheDocument();
 });
---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Hello, world!</header>
+    </div>
+  );
+}
+
+export default App;
