ec7aa6d907ebcd1a53fccef1317cef8fa805bac2
Vada Karlen <vada.karlen@gmail.com>
2021-08-25 01:36:43 -0500

    Added basic framework for Arcade page

--- src/ArcadePage/ArcadePage.jsx
+++ src/ArcadePage/ArcadePage.jsx
@@ -1,5 +1,24 @@
+import { useHistory } from 'react-router-dom';
+
 function ArcadePage() {
-  return <div>Arcade Page</div>;
+  const history = useHistory();
+
+  return (
+    <div>
+      <h1>Arcade</h1>
+      <button onClick={() => history.push('/')}>Back</button>
+      <div>GAME GO HERE</div>
+      <div>
+        <h2>Directions</h2>
+        <p>Its time to escape, Boris! You better get going!</p>
+        <ul>
+          <li>L/R, A/D - Move left and right</li>
+          <li>Space - Jump</li>
+          <li>Shift - Crouch</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default ArcadePage;
