0f9a8cc876a38167a55e8d619399e21d13d002cc
Vada Karlen <vada.karlen@gmail.com>
2021-08-28 23:36:35 -0500

    Fixed unused vars

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ import grant from './images/grant.jpeg';
 import vada from './images/vada.jpeg';
 import bem from './images/bem.jpeg';
 
-import { Button, Card, CardGroup, ListGroup, Container, Row, Col, Image} from 'react-bootstrap';
+import { Button, Card, Container, Row, Col, Image } from 'react-bootstrap';
 
 const LandingPage = () => {
   const history = useHistory();
@@ -24,25 +24,25 @@ const LandingPage = () => {
 
       <Row>
         <Col>
-          <Card
-            className="mb-2"
-            style={{ padding: '15px' } }
-            bg=""
-            text="black"
-            >
+          <Card className="mb-2" style={{ padding: '15px' }} bg="" text="black">
             <Card.Title>Hail, Adventurer!</Card.Title>
             <Card.Text>
-            <p>Master Skeletor has ruled over this decrepit place for 1,000 years and
-            nary a prisoner has escaped. The evil mastermind has ruled with a bony
-            fist, supported by his skeletal minions. Adventurers often arrive to
-            seek fame in challenging the lord of the dungeon and setting the
-            prisoners free, but none have succeeded.</p>
+              <p>
+                Master Skeletor has ruled over this decrepit place for 1,000
+                years and nary a prisoner has escaped. The evil mastermind has
+                ruled with a bony fist, supported by his skeletal minions.
+                Adventurers often arrive to seek fame in challenging the lord of
+                the dungeon and setting the prisoners free, but none have
+                succeeded.
+              </p>
 
-            <p>However, our hero Boris Bones is tired of the dampness of this cave.
-            Tired by the constant seiges. Tired of the non-existent pay... just
-            all over tired of this life! Or, well. Life so to speak. Its time he
-            broke out of this evil lair once and for all, and felt the sun on his
-            boney face!</p>
+              <p>
+                However, our hero Boris Bones is tired of the dampness of this
+                cave. Tired by the constant seiges. Tired of the non-existent
+                pay... just all over tired of this life! Or, well. Life so to
+                speak. Its time he broke out of this evil lair once and for all,
+                and felt the sun on his boney face!
+              </p>
             </Card.Text>
           </Card>
         </Col>
@@ -65,10 +65,12 @@ const LandingPage = () => {
       <Row className="justify-content-around">
         <Col>
           <Card className="text-center" style={{ width: '11rem' }}>
-            <Image src={bem} roundedCircle/>
+            <Image src={bem} roundedCircle />
 
             <Card.Body>
-              <Card.Title style={{ backgroundColor: 'gray',  }}>Bermet Tariel</Card.Title>
+              <Card.Title style={{ backgroundColor: 'gray' }}>
+                Bermet Tariel
+              </Card.Title>
               <Card.Link href="linkedin.com/in/bermet-tariel">
                 <i class="bi bi-linkedin"></i>
                 <svg
@@ -78,7 +80,7 @@ const LandingPage = () => {
                   fill="black"
                   class="bi bi-linkedin"
                   viewBox="0 0 16 16"
-                  >
+                >
                   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                 </svg>
               </Card.Link>
@@ -92,7 +94,7 @@ const LandingPage = () => {
                   fill="black"
                   class="bi bi-github"
                   viewBox="0 0 16 16"
-                  >
+                >
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                 </svg>
               </Card.Link>
@@ -102,9 +104,11 @@ const LandingPage = () => {
 
         <Col>
           <Card className="text-center" style={{ width: '11rem' }}>
-            <Image src={vada} roundedCircle/>
-            <Card.Body >
-              <Card.Title style={{ backgroundColor: 'gray' }}>Vada Karlen</Card.Title>
+            <Image src={vada} roundedCircle />
+            <Card.Body>
+              <Card.Title style={{ backgroundColor: 'gray' }}>
+                Vada Karlen
+              </Card.Title>
 
               <Card.Link href="https://www.linkedin.com/in/vadak/">
                 <i class="bi bi-linkedin"></i>
@@ -115,7 +119,7 @@ const LandingPage = () => {
                   fill="black"
                   class="bi bi-linkedin"
                   viewBox="0 0 16 16"
-                  >
+                >
                   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                 </svg>
               </Card.Link>
@@ -129,7 +133,7 @@ const LandingPage = () => {
                   fill="black"
                   class="bi bi-github"
                   viewBox="0 0 16 16"
-                  >
+                >
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                 </svg>
               </Card.Link>
@@ -139,10 +143,12 @@ const LandingPage = () => {
 
         <Col>
           <Card className="text-center" style={{ width: '11rem' }}>
-            <Image  src={grant} roundedCircle/>
+            <Image src={grant} roundedCircle />
 
-            <Card.Body >
-              <Card.Title style={{ backgroundColor: 'gray' }}>Grant Nichols</Card.Title>
+            <Card.Body>
+              <Card.Title style={{ backgroundColor: 'gray' }}>
+                Grant Nichols
+              </Card.Title>
 
               <Card.Link href="https://www.linkedin.com/in/grant-nichols-dev/">
                 <i class="bi bi-linkedin"></i>
@@ -153,7 +159,7 @@ const LandingPage = () => {
                   fill="black"
                   class="bi bi-linkedin"
                   viewBox="0 0 16 16"
-                  >
+                >
                   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                 </svg>
               </Card.Link>
@@ -167,13 +173,13 @@ const LandingPage = () => {
                   fill="black"
                   class="bi bi-github"
                   viewBox="0 0 16 16"
-                  >
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                  </svg>
-                </Card.Link>
-              </Card.Body>
-            </Card>
-          </Col>
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
