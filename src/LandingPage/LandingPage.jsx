c9a54d8bd988178c02e5db84d614b953067650a6
Vada Karlen <vada.karlen@gmail.com>
2021-08-28 19:50:39 -0500

    Updated Story

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,17 +1,17 @@
 import { useHistory } from 'react-router-dom';
-import tempBoris from "./images/tempBoris.gif"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import tempBoris from './images/tempBoris.gif';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import pic from "./images/pic.jpg"
-import grant from "./images/grant.jpg"
-import vada from "./images/vada.jpg"
-import bem from "./images/bem.jpeg"
+import grant from './images/grant.jpg';
+import vada from './images/vada.jpg';
+import bem from './images/bem.jpeg';
 // import Button from 'react-bootstrap/Button'
 // import Figure from 'react-bootstrap/Figure'
-import logo from './images/logo.jpeg'
+import logo from './images/logo.jpeg';
 // import DropdownButton from 'react-bootstrap/DropdownButton'
 // import Dropdown from 'react-bootstrap/Dropdown'
 // import { className } from 'postcss-selector-parser';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
 // import Image from 'react-bootstrap/Image'
 // import { height } from 'dom-helpers';
 
@@ -21,19 +21,24 @@ const LandingPage = () => {
   return (
     <div className="container">
       <div className="logo">
-        <img className='logoPic' src={logo} alt="logoPic"/>
-        <h2 id='title'> Game Title</h2>
+        <img className="logoPic" src={logo} alt="logoPic" />
+        <h2 id="title">The Peaceful Pursuit of Boris Bones</h2>
       </div>
       <div>
         <h3>Story</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Hail, Adventurer! Master Skeletor has ruled over this decrepit place
+          for 1,000 years and nary a prisoner has escaped. The evil mastermind
+          has ruled with a bony fist, supported by his skeletal minions.
+          Adventurers often arrive to seek fame in challenging the lord of the
+          dungeon and setting the prisoners free, but none have succeeded.
+        </p>
+        <p>
+          However, our hero Boris Bones is tired of the dampness of this cave.
+          Tired by the constant seiges. Tired of the non-existent pay... just
+          all over tired of this life! Or, well. Life so to speak. Its time he
+          broke out of this evil lair once and for all, and felt the sun on his
+          boney face!
         </p>
       </div>
 
@@ -41,47 +46,54 @@ const LandingPage = () => {
         <img src={tempBoris} alt="Our hero, Boris" />
       </div>
       <div className="mb-2">
-      <button className="btn" onClick={() => history.push('/arcade')}>Play Game</button>
+        <button className="btn" onClick={() => history.push('/arcade')}>
+          Play Game
+        </button>
       </div>
 
       <div className="block">
         <div>
-      <Card style={{ width: '18rem'}}>
-        <Card.Img variant="top" src={bem} />
-        <Card.Body>
-          <Card.Title>Bermet Tariel</Card.Title>
-        </Card.Body>
-        <Card.Body>
-          <Card.Link href="https://www.linkedin.com/in/bermet-tariel/">linkedin</Card.Link>
-        </Card.Body>
-      </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={bem} />
+            <Card.Body>
+              <Card.Title>Bermet Tariel</Card.Title>
+            </Card.Body>
+            <Card.Body>
+              <Card.Link href="https://www.linkedin.com/in/bermet-tariel/">
+                linkedin
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={vada} />
+            <Card.Body>
+              <Card.Title>Vada Karlen</Card.Title>
+            </Card.Body>
+            <Card.Body>
+              <Card.Link href="https://www.linkedin.com/in/vadak/">
+                linkedin
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </div>
+        <div>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={grant} />
+            <Card.Body>
+              <Card.Title>Grant Nichols</Card.Title>
+            </Card.Body>
+            <Card.Body>
+              <Card.Link href="https://www.linkedin.com/in/grant-nichols-dev/">
+                linkedin
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-      <div>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={vada} />
-        <Card.Body>
-          <Card.Title>Vada Karlen</Card.Title>
-        </Card.Body>
-        <Card.Body>
-          <Card.Link href="https://www.linkedin.com/in/vadak/">linkedin</Card.Link>
-        </Card.Body>
-      </Card>
-      </div>
-      <div>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={grant} />
-        <Card.Body>
-          <Card.Title>Grant Nichols</Card.Title>
-        </Card.Body>
-        <Card.Body>
-          <Card.Link href="https://www.linkedin.com/in/grant-nichols-dev/">linkedin</Card.Link>
-        </Card.Body>
-      </Card>
-      </div>   
-      </div>
-
     </div>
   );
-}
+};
 
 export default LandingPage;
