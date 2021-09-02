7c05135ef524a54fdd5665cc5c896c10eb0cba3c
Vada Karlen <vada.karlen@gmail.com>
2021-09-02 03:57:27 -0500

    Moves tech used to drawer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -14,6 +14,10 @@ import {
 import grant from './images/grant.jpeg';
 import vada from './images/vada.jpeg';
 import bem from './images/bem.jpeg';
+import bp from './images/bs.jpeg';
+import jt from './images/js.jpeg';
+import km from './images/kaboomjs.jpg';
+import rt from './images/react.png';
 
 function Footer() {
   const [show, setShow] = useState(false);
@@ -23,7 +27,7 @@ function Footer() {
 
   return (
     <>
-      <svg
+      {/* <svg
         xmlns="http://www.w3.org/2000/svg"
         width="58"
         height="58"
@@ -34,6 +38,26 @@ function Footer() {
         id="open-btn"
       >
         <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+      </svg> */}
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="58"
+        height="58"
+        fill="#fff"
+        class="bi bi-box-arrow-left"
+        viewBox="0 0 16 16"
+        onClick={handleShow}
+        id="open-btn"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
+        />
+        <path
+          fill-rule="evenodd"
+          d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
+        />
       </svg>
 
       <div id="git-btn">
@@ -170,6 +194,37 @@ function Footer() {
                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                   </svg>
                 </a>
+              </Col>
+            </Row>
+
+            <Row>
+              <h2 id="tech-head">Technologies Used</h2>
+
+              <Col xs={12} className="mt-2">
+                <Container className="text-center">
+                  <Row>
+                    <Col xs={1} md={3}>
+                      <a href="https://reactjs.org/">
+                        <Image src={rt} className="tech-icon" rounded />
+                      </a>
+                    </Col>
+                    <Col xs={1} md={3}>
+                      <a href="https://www.javascript.com/">
+                        <Image src={jt} className="tech-icon" rounded />
+                      </a>
+                    </Col>
+                    <Col xs={1} md={3}>
+                      <a href="https://getbootstrap.com/">
+                        <Image src={bp} className="tech-icon" rounded />
+                      </a>
+                    </Col>
+                    <Col xs={1} md={3}>
+                      <a href="https://kaboom.org/">
+                        <Image src={km} className="tech-icon" rounded />
+                      </a>
+                    </Col>
+                  </Row>
+                </Container>
               </Col>
             </Row>
           </Container>
--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,11 +1,7 @@
 import { useHistory } from 'react-router-dom';
 import React from 'react';
 
-import borisPic from './images/BorisPic.png';
-import bp from './images/bs.jpeg';
-import jt from './images/js.jpeg';
-import km from './images/kaboomjs.jpg';
-import rt from './images/react.png';
+import vid from './images/Boris.mov';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Card, Container, Row, Col, Image } from 'react-bootstrap';
@@ -21,75 +17,50 @@ const LandingPage = () => {
         </header>
       </Row>
 
-      <Row className="align-items-stretch">
-        <Col className="m-3" xs={7}>
-          <Card id="story-container" text="black">
-            <Card.Title>Hail, Adventurer!</Card.Title>
-            <Card.Text>
-              <p>
-                Master Skeletor has ruled over this decrepit dungeon for 1,000
-                years and nary a prisoner has escaped. The evil mastermind has
-                ruled with a bony fist, supported by his skeletal minions.
-                Adventurers often arrive to seek fame in challenging the lord of
-                the dungeon and setting the prisoners free, but none have
-                succeeded.
-              </p>
+      <Row className="align-items-stretch justify-content-center">
+        <Col className="m-3 " xs={5}>
+          <Row>
+            <Card id="story-container" text="black">
+              <Card.Title>Hail, Adventurer!</Card.Title>
+              <Card.Text>
+                <p>
+                  Master Skeletor has ruled over this decrepit dungeon for 1,000
+                  years and nary a prisoner has escaped. The evil mastermind has
+                  ruled with a bony fist, supported by his skeletal minions.
+                  Adventurers often arrive to seek fame in challenging the lord
+                  of the dungeon and setting the prisoners free, but none have
+                  succeeded.
+                </p>
 
-              <p>
-                However, our hero Boris Bones is tired of the dampness of this
-                cave. Tired by the constant seiges. Tired of the non-existent
-                pay... just all over tired of this life! Or, well. Life so to
-                speak. Its time he broke out of this evil lair once and for all,
-                and felt the sun on his boney face!
-              </p>
-            </Card.Text>
-          </Card>
-
-          <h2 id="tech-header">Technologies Used</h2>
-
-          <Col xs={12} className="mt-2">
-            <Card className="p-2">
-              <Container className="text-center">
-                <Row>
-                  <Col xs={1} md={3}>
-                    <Card.Link href="https://reactjs.org/">
-                      <Image src={rt} className="tech-icon" rounded />
-                    </Card.Link>
-                  </Col>
-                  <Col xs={1} md={3}>
-                    <Card.Link href="https://www.javascript.com/">
-                      <Image src={jt} className="tech-icon" rounded />
-                    </Card.Link>
-                  </Col>
-                  <Col xs={1} md={3}>
-                    <Card.Link href="https://getbootstrap.com/">
-                      <Image src={bp} className="tech-icon" rounded />
-                    </Card.Link>
-                  </Col>
-                  <Col xs={1} md={3}>
-                    <Card.Link href="https://kaboom.org/">
-                      <Image src={km} className="tech-icon" rounded />
-                    </Card.Link>
-                  </Col>
-                </Row>
-              </Container>
+                <p>
+                  However, our hero Boris Bones is tired of the dampness of this
+                  cave. Tired by the constant seiges. Tired of the non-existent
+                  pay... just all over tired of this life! Or, well. Life so to
+                  speak. Its time he broke out of this evil lair once and for
+                  all, and felt the sun on his boney face!
+                </p>
+              </Card.Text>
             </Card>
-          </Col>
+          </Row>
+
+          <Row className="mt-4">
+            <Button
+              variant="danger"
+              size="lg"
+              onClick={() => history.push('/arcade')}
+            >
+              Play Game
+            </Button>
+          </Row>
         </Col>
 
-        <Col className="m-3 landing-pic" xs={4}>
-          <img src={borisPic} alt="Our hero, Boris" />
+        <Col className="m-3 landing-pic" xs={5}>
+          <video controls autoPlay muted loop>
+            <source src={vid} type="video/mp4" />
+            <source src={vid} type="video/ogg" />
+            Your browser does not support the video tag.
+          </video>
         </Col>
-      </Row>
-
-      <Row className="m-3">
-        <Button
-          variant="danger"
-          size="lg"
-          onClick={() => history.push('/arcade')}
-        >
-          Play Game
-        </Button>
       </Row>
     </Container>
   );
