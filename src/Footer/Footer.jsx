e784a1a61fe84435bff327f9b9a0411e73fa1cd8
Vada Karlen <vada.karlen@gmail.com>
2021-09-02 22:22:26 -0500

    Changed the dev button

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -26,38 +26,23 @@ function Footer() {
 
   return (
     <>
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="58"
-        height="58"
-        fill="#fff"
-        class="bi bi-arrow-left-circle-fill"
-        viewBox="0 0 16 16"
-        onClick={handleShow}
-        id="open-btn"
+      <OverlayTrigger
+        placement="left"
+        overlay={<Tooltip id={`tooltip-right`}>Meet the developers</Tooltip>}
       >
-        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
-      </svg> */}
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="58"
-        height="58"
-        fill="#fff"
-        class="bi bi-box-arrow-left"
-        viewBox="0 0 16 16"
-        onClick={handleShow}
-        id="open-btn"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
-        />
-        <path
-          fill-rule="evenodd"
-          d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
-        />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="58"
+          height="58"
+          fill="#fff"
+          class="bi bi-person-lines-fill"
+          viewBox="0 0 16 16"
+          onClick={handleShow}
+          id="open-btn"
+        >
+          <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+        </svg>
+      </OverlayTrigger>
 
       <div id="git-btn">
         <OverlayTrigger
